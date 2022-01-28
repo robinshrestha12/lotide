8357ccffb5d2e3ab39c902670fa4fd8ebedb21e9
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-01-28 13:24:06 -0500

    assertArraysEqual complete

---
+++ assertArraysEqual.js
@@ -0,0 +1,31 @@
+function eqArrays(array1, array2) {
+  //console.log(array2.length);
+  //console.log(array1.length);
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  else {
+    for (let i = 0; i < array2.length; i++) {
+      if (typeof array1[i] !== typeof array2[i]) {
+        return false;
+      }
+      else {
+        if (array1[i] !== array2[i]) {
+          return false;
+        }
+      }
+    }
+  }
+  return true;
+}
+const assertArraysEqual = function (arrayOne, arrayTwo) {
+  if (eqArrays(arrayOne, arrayTwo) === true) {
+    console.log(`✅✅✅ Assertion Passed: ${arrayOne} === ${arrayTwo}`);
+  } else {
+    console.log(`🛑🛑🛑 Assertion Failed: ${arrayOne} !== ${arrayTwo}`);
+  }
+}
+assertArraysEqual([1, 2, 3], [1, 2, 3]);
+assertArraysEqual([1, 2, 3], [3, 2, 1]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", "3"]);
+assertArraysEqual(["1", "2", "3"], ["1", "2", 3]);
