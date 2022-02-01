8840171a73e8965444294c2a3750ee1287abd195
robinshrestha12 <robinshrestha.maple@gmail.com>
2022-02-01 10:17:19 -0500

    findKeyByValue complete

---
+++ findKeyByValue.js
@@ -0,0 +1,34 @@
+const assertEqual = function (actual, expected) {
+  if (((typeof (actual) === "number")) && ((typeof (expected) === "number"))) {
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+    }
+  } else {
+    if (actual === expected) {
+      console.log(`✅✅✅ Assertion Passed: "${actual}" === "${expected}"`);
+    } else {
+      console.log(`🛑🛑🛑 Assertion Failed: "${actual}" !== "${expected}"`);
+    }
+  }
+};
+const findKeyByValue = function (objectInput, valueInput) {
+  let keyFound = "";
+  //iterating over the keys
+  for (const key of Object.keys(objectInput)) {
+    //comparing object values with given values
+    if (objectInput[key] === valueInput) {
+      keyFound = key;
+      return keyFound;
+    }
+  }
+};
+const bestTVShowsByGenre = {
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama: "The Wire"
+};
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
